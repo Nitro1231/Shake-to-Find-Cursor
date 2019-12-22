f516ced804b95d79aae3d6ae68a6c6729a4b0070
Nitro <nitro0@naver.com>
2019-12-23 01:16:40 +0900

    System were up to date

--- Shake-to-Find-Cursor/Shake-to-Find-Cursor/Cursor.cs
+++ Shake-to-Find-Cursor/Shake-to-Find-Cursor/Cursor.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Shake_to_Find_Cursor {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class Cursor : Form {
+        public Cursor() {
             InitializeComponent();
+        }
+
+        private void Cursor_Load(object sender, EventArgs e) {
+            
         }
     }
 }
--- Shake-to-Find-Cursor/Shake-to-Find-Cursor/Program.cs
+++ Shake-to-Find-Cursor/Shake-to-Find-Cursor/Program.cs
@@ -13,7 +13,7 @@ namespace Shake_to_Find_Cursor {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Cursor());
         }
     }
 }
